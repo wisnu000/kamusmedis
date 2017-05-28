e7d2f4969f66c994a31fd57fce0af3318db19a98
wisnu000 <wisnu7884@students.amikom.ac.id>
2017-05-28 16:49:20 +0700

    edit KamusActivity dan penambahan file xml

--- app/src/main/java/com/philos/kamusmedis/KamusActivity.java
+++ app/src/main/java/com/philos/kamusmedis/KamusActivity.java
@@ -1,13 +1,90 @@
 package com.philos.kamusmedis;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class KamusActivity extends AppCompatActivity {
+
+    private TextView artiKata;
+    private TextToSpeech convertToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kamus);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        int kamusId = bundle.getInt("KAMUS_ID");
+        int id = kamusId + 1;
+
+        TextView kata = (TextView)findViewById(R.id.kata);
+        artiKata = (TextView)findViewById(R.id.kamus);
+        Button textToSpeech = (Button)findViewById(R.id.button);
+
+        DbBackend dbBackend = new DbBackend(KamusActivity.this);
+        KamusObject allKamusQuestions = dbBackend.getKamusById(id);
+
+        kata.setText(allKamusQuestions.getKata());
+        artiKata.setText(allKamusQuestions.getDefinisi());
+
+        textToSpeech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String convertTextToSpeech = artiKata.getText().toString();
+                convertToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void onInit(int status) {
+                        if(status != TextToSpeech.ERROR){
+                            convertToSpeech.setLanguage(Locale.US);
+                            convertToSpeech.speak(convertTextToSpeech, TextToSpeech.QUEUE_FLUSH, null, null);
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dictionary, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        if(convertToSpeech != null){
+            convertToSpeech.stop();
+            convertToSpeech.shutdown();
+        }
+        super.onPause();
     }
 }
